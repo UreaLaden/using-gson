f001b825ef002d065f20eaacf02b1a48407d3a6d
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-04-02 11:06:22 -0400

    Fixed functionality

--- app/src/main/java/using/gson/App.java
+++ app/src/main/java/using/gson/App.java
@@ -8,79 +8,80 @@ import com.google.gson.Gson;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class App {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-//        System.out.println(App.getQuoteFromAPI());// why do hate me correitio
-//        getQuoteFromAPI();
-        Qoute newQoute = new Qoute("jim","hello hello hello","never trust a fart","tag1");
-
-        System.out.println("hello world");
-        addToJSON(newQoute);
-
-
+        ArrayList<Qoute> quoteList = new ArrayList<>();
+        quoteList = getQuotesFromJSON(quoteList);
+        addToJSON(quoteList,getQuoteFromAPI());
+//        addToJSON(quoteList,getQuoteFromAPI());
+//        System.out.println(getQuoteFromAPI());
     }
 
-    public static String getQuoteFromJSON() throws FileNotFoundException {
+    public static ArrayList<Qoute> getQuotesFromJSON(ArrayList<Qoute> quoteList){
         Gson gson = new Gson();
         String path = "/mnt/Ubuntu_Data/CodeFellows/Labs/java/using-gson/app/src/main/resources/quotes.json";
         File file = new File(path);
-        FileReader reader = new FileReader(file);
+        FileReader reader = null;
+
+        try
+        {
+            reader = new FileReader(file);
+        }
+        catch(FileNotFoundException c){c.printStackTrace();}
+        assert reader != null;
         Qoute[] qoutes = gson.fromJson(reader, Qoute[].class);
         int max = qoutes.length;
-        int rand = (int)(Math.random()*max);
-        return qoutes[rand].toString();
+        int rand = (int)(Math.random()* max);
+        for(Qoute q:qoutes){
+            if(q != null)
+                quoteList.add(q);
+        }
+        return quoteList;
+
     }
 
-    public static <Quote> String getQuoteFromAPI() throws IOException {
-        String starWarsURL = "https://swapi.dev/api/people/?search=r2";
-        String pokeUrl  = "https://pokeapi.co/api/v2/pokemon/ditto";
-        String programmerURL = "http://quotes.stormconsultancy.co.uk/popular.json";
-        URL url = new URL(programmerURL);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        System.out.println(connection);
-        InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
-        System.out.println(inputStreamReader);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        String jsonLine = bufferedReader.readLine();
-//        System.out.println(jsonLine);
+    public static Qoute getQuoteFromAPI(){
         Gson gson = new Gson();
+        String jsonLine = "";
+        try{
+            String programmerURL = "http://quotes.stormconsultancy.co.uk/popular.json";
+            URL url = new URL(programmerURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            jsonLine = bufferedReader.readLine();
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
         Qoute[] results = gson.fromJson(jsonLine,Qoute[].class);
-        System.out.println(Arrays.toString(results));
-        System.out.println(results[0]);
-//        String res = results.toString();
-
-
-
-
-        return programmerURL;
+        int randomNum = (int)(Math.random() * results.length);
+        Qoute randomQuote = results[randomNum];
+        System.out.println(randomQuote.toString());
+        return randomQuote;
     }
 
 
-    public static void addToJSON(Qoute qoute) throws IOException {
+    public static void addToJSON(ArrayList<Qoute> quoteList,Qoute qoute){
         Gson gson = new Gson();
-        String newPath = "/home/owner/codefellows/using-gson/app/src/main/resources/miniQuotes.json";
-//        ArrayList<Qoute> qouteArrayList = new  ArrayList<>();
-
-
-//        saveQuote = new Quote[] {new Qoute("jim","hello hello hello","never trust a fart")};
+        String newPath = "/mnt/Ubuntu_Data/CodeFellows/Labs/java/using-gson/app/src/main/resources/miniQuotes.json";
         File file = new File(newPath );
-        try {
-            FileReader fileRead = new FileReader(file);
-            Qoute newQ = gson.fromJson(fileRead, Qoute.class);
-
-            System.out.println(newQ);
+        quoteList.add(qoute);
+        FileWriter fileWriter = null;
+        try{
+            fileWriter = new FileWriter(file);
+            gson.toJson(quoteList, fileWriter);
+            fileWriter.close();
+        }catch(IOException e)
+        {
+            e.printStackTrace();
         }
-        catch (Exception MalformedJsonException ){
-            System.out.println("dont work bro");
-        }
-        FileWriter fileWriter = new FileWriter(file,true);
-        gson.toJson(qoute, fileWriter);
-        fileWriter.close();
-//        System.out.println(qoute);
 
     }
 
--- app/src/main/java/using/gson/Qoute.java
+++ app/src/main/java/using/gson/Qoute.java
@@ -1,40 +1,32 @@
 package using.gson;
 
 public class Qoute {
-//    private static
     String author;
     String text;
     String quote;
-
-    String tags;
-
+    String[] tags;
 
 
-    public Qoute(String author,String text, String quote, String tags) {
-//        this.setAuthor(author);
+    public Qoute(String author, String text, String quote, String[] tags) {
         this.author = author;
         this.text = text;
         this.quote = quote;
-
         this.tags = tags;
+    }
 
+    @Override
+    public String toString() {
+        return "Qoute{" +
+                "author='" + author + '\'' +
+                ", text='" + text + '\'' +
+                ", quote='" + quote + '\'' +
+                ", tags='" + tags + '\'' +
+                '}';
     }
 
     public  String getAuthor() {
         return author;
     }
 
-
-
-
-    @Override
-    public String toString() {
-
-        return "Qoute{" +
-                "author='" + getAuthor() + '\'' +
-                ", quote= " + (text != null?text:"")  + '\'' +
-                (quote != null ? quote:"") + '\'' +
-                '}';
-    }
 
 }
